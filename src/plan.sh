962290fc063aa8a274b21c22a6a9b8b7e05114d9
Adam Green <adam.green@kcl.ac.uk>
2021-03-09 12:35:47 +0000

    fixed a minor major termination bug

--- src/plan.sh
+++ src/plan.sh
@@ -6,7 +6,7 @@ do
   echo '\n\n' >> ../out.txt
   echo '```' >> ../out.txt
   # Note -x600 indicates a 600sec time execution cap, if you lift this, GitHub Actions might freak out
-  optic -x600 ./domain.pddl ./problem_$x.pddl >> ../out.txt
+  optic -N -x600 ./domain.pddl ./problem_$x.pddl >> ../out.txt
   echo '```' >> ../out.txt
   echo '\n\n' >> ../out.txt
   x=$(( $x + 1 ))
